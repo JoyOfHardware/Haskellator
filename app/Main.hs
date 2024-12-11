e295647bbe161b0b572733ce104b01d44a744dcd
Yehowshua Immanuel <yehowshua@yehowshuaimmanuel.com>
2024-12-10 20:20:29 -0500

    🎵greater things are yet to come🎵

--- app/Main.hs
+++ app/Main.hs
@@ -17,7 +17,6 @@ main = do
         (filePath:_) -> do
             -- Attempt to read the file
             contents <- catch (readFile filePath) handleReadError
-            putStrLn "File Contents:"
             putStrLn $ ppShow $ Haskellator.runParser contents filePath
         [] -> putStrLn "cabal run Haskellator -- <file-path>"
     -- putStrLn $ ppShow Haskellator.val
