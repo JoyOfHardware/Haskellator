c919b8fb41234762904b917ddbcbf7f344b79b66
Artturin <Artturin@artturin.com>
2024-12-11 22:06:10 +0200

    Update nixpkgs

--- shell.nix
+++ shell.nix
@@ -1,6 +1,9 @@
-{ pkgs ? import (fetchTarball {
-  url = "https://github.com/NixOS/nixpkgs/archive/refs/tags/24.05.tar.gz";
-}) {} }:
+{
+  pkgs ? import (fetchTarball {
+    # nixos-unstable rev 2024-12-03
+    url = "https://github.com/NixOS/nixpkgs/archive/55d15ad12a74eb7d4646254e13638ad0c4128776.tar.gz";
+  }) { },
+}:
 
 let
   haskellator = pkgs.callPackage ./default.nix {};
